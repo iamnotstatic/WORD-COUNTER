8e1a9bf27ebee97202d90a23defa043c1c38b4d4
Abdul-fatai <staticdev20046@gmail.com>
2019-10-01 13:34:09 +0100

    main js modify

--- main.js
+++ main.js
@@ -5,12 +5,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
  ** - Shows number of characters, words, sentences, paragraphs - Done
  ** - Show reading time - Done
  ** - Show keyword count - Done
- ** - Show reading level (Optional - how?) - Done
  ** - Above data should change/appear on every keypress - Done
  **
  */
-
-// Readability (Mashape) API Key for testing: PQ4FOFuaR6mshI6qpnQKQvkDZQXjp1o6Zcqjsnug7GvNggTzUE
 
 "use strict";
 var input = document.querySelectorAll('textarea')[0],
@@ -139,64 +136,4 @@ input.addEventListener('keyup', function() {
 
 });
 
-// readability level using readability-metrics API from Mashape
-readability.addEventListener('click', function() {
-
-  // placeholder until the API returns the score  
-  readability.innerHTML = "Fetching score...";
-
-  var requestUrl = "https://ipeirotis-readability-metrics.p.mashape.com/getReadabilityMetrics?text=";
-  var data = input.value;
-
-  var request = new XMLHttpRequest();
-  request.open('POST', encodeURI(requestUrl + data), true);
-  request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-  request.setRequestHeader("X-Mashape-Authorization", "PQ4FOFuaR6mshI6qpnQKQvkDZQXjp1o6Zcqjsnug7GvNggTzUE");
-  request.send();
-
-  request.onload = function() {
-    if (this.status >= 200 && this.status < 400) {
-      // Success!
-      readability.innerHTML = readingEase(JSON.parse(this.response).FLESCH_READING);
-    } else {
-      // We reached our target server, but it returned an error
-      readability.innerHTML = "Not available.";
-    }
-  };
-
-  request.onerror = function() {
-    // There was a connection error of some sort
-    readability.innerHTML = "Not available.";
-  };
-});
-
-// function to convert FLESCH READING SCORE into meaningful string.
-function readingEase(num) {
-  switch (true) {
-    case (num <= 30):
-      return "Readability: College graduate.";
-      break;
-    case (num > 30 && num <= 50):
-      return "Readability: College level.";
-      break;
-    case (num > 50 && num <= 60):
-      return "Readability: 10th - 12th grade.";
-      break;
-    case (num > 60 && num <= 70):
-      return "Readability: 8th - 9th grade.";
-      break;
-    case (num > 70 && num <= 80):
-      return "Readability: 7th grade.";
-      break;
-    case (num > 80 && num <= 90):
-      return "Readability: 6th grade.";
-      break;
-    case (num > 90 && num <= 100):
-      return "Readability: 5th grade.";
-      break;
-    default:
-      return "Not available.";
-      break;
-  }
-}
 });
